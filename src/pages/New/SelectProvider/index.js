b73076eac91dae5aa5ee086c5b6df3d4e79c3ec5
Djanilson Martins <djanlm@hotmail.com>
2019-10-28 20:33:05 -0300

    providers_list

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '~/services/api';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
 
-export default function SelectProvider() {
-  return <Background />;
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+      setProviders(response.data);
+    }
+    loadProviders();
+  }, []);
+
+  function formatImageURL(url) {
+    const urlSplitted = url.split(''); // turns string into an array so that we can use the splice method
+    urlSplitted.splice(
+      7,
+      9,
+      ['1'],
+      ['0'],
+      ['.'],
+      ['0'],
+      ['.'],
+      ['2'],
+      ['.'],
+      ['2']
+    );
+    return urlSplitted.join('');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? formatImageURL(provider.avatar.url)
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
