e07cfb908ba725c259507a596ea1aa52a6dbc379
Djanilson Martins <djanlm@hotmail.com>
2019-10-29 06:49:24 -0300

    confirm appointment

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,74 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { formatRelative, parseISO } from 'date-fns';
+// import pt from 'date-fns/locale/pt';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+import Background from '~/components/Background';
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
 
-export default function Confirm() {
-  return <View />;
+export default function Confirm({ navigation }) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date()),
+    [time]
+  );
+
+  function formatImageURL(url) {
+    const urlSplitted = url.split(''); // turns string into an array so that we can use the splice method
+    urlSplitted.splice(
+      7,
+      9,
+      ['1'],
+      ['0'],
+      ['.'],
+      ['0'],
+      ['.'],
+      ['2'],
+      ['.'],
+      ['2']
+    );
+    return urlSplitted.join('');
+  }
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? formatImageURL(provider.avatar.url)
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+        <SubmitButton onPress={handleAddAppointment}>Confirm</SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirm appointment',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
